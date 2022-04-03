865d8393d4fd3ba99cddcc32d1b956845982d028
elzup <guild0105@gmail.com>
2022-04-03 18:47:56 +0900

    update check

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.900, auto_write=False, pixel_order=ORDER)
 for i in range(num_pixels):
-    pixels[i] = (255, 0, 0)
+    pixels[i] = (0, 0, 0)
+    if i % 20 == 0:
+        pixels[i] = (100, 100, 0)
+    elif i % 10 == 0:
+        pixels[i] = (100, 0, 0)
+
 pixels.show()
