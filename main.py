43cbe497cd427b5a725065a11a82a726cd82a478
elzup <guild0105@gmail.com>
2022-04-03 20:00:06 +0900

    update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import neopixel
 
 pixel_pin = board.D18
 num_pixels = 300
-ORDER = neopixel.GRB
+ORDER = neopixel.RGB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.900, auto_write=False, pixel_order=ORDER)
-for i in range(num_pixels):
-    pixels[i] = (0, 0, 0)
-    if i % 20 == 0:
-        pixels[i] = (100, 100, 0)
-    elif i % 10 == 0:
-        pixels[i] = (100, 0, 0)
+    pixel_pin, num_pixels, brightness=0.900, pixel_order=ORDER)
+
+pixels.fill((0, 0, 0))
+
+# for i in range(num_pixels):
+#    if i % 20 == 0:
+#        pixels[i] = (100, 100, 0)
+#    elif i % 10 == 0:
+#        pixels[i] = (100, 0, 0)
+
+pixels[100] = (100, 0, 0)
+# pixels[200] = (100, 0, 0)
 
 pixels.show()
